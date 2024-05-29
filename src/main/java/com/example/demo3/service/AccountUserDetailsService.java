fbc9e04b1080c67530739455f1ace17a5d0b26ac
suzukiyota <169060298+1001yyy1001@users.noreply.github.com>
2024-05-29 15:48:57 +0900

    質問用

--- src/main/java/com/example/demo3/service/AccountUserDetailsService.java
+++ src/main/java/com/example/demo3/service/AccountUserDetailsService.java
@@ -1,5 +1,30 @@
 package com.example.demo3.service;
 
-public class AccountUserDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.example.demo3.data.repository.PostsRepository;
+
+@Service // Spring管理Beanであることを指定
+public class AccountUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private PostsRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		if (userName == null || "".equals(userName)) {
+			throw new UsernameNotFoundException("ユーザー名が空です");
+		}
+		// データベースからアカウント情報を取得する
+		Users user = repository.findById(userName).get();
+		if (user != null) {
+			// UserDetailsの実装クラスを生成して返す
+			return new AccountUserDetails(user);
+		}
+		throw new UsernameNotFoundException(userName + "は見つかりません。");
+	}
 }
